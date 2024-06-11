b6f8a29b73edccb67794290cb5c2ac3d6af202f5
John Knotts <jhnnyk@gmail.com>
2024-06-11 13:23:47 -0600

    using previous state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import './App.css';
-import { useState } from 'react';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
   // useState returns a variable and a function inside an array
   // we use array destructuring to capture those
-  const [name, setName] = useState('mario');
   const [events, setEvents] = useState([
     {title: "mario's birthday bash", id: 1},
     {title: "bowser's live stream", id: 2},
     {title: 'race on moo moo farm', id: 3}
   ])
 
-  const handleClick = () => {
-    setName('luigi');
-    console.log(name);
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
   }
+  // when updating state depends on the previous state, it is best practice to do it like this
+  // to prevent weird things
 
   return (
     <div className="App">
-      <h1>My name is {name}!</h1>
-      <button onClick={handleClick}>change name</button>
 
       {events.map((event) => (
         <div key={event.id}>
           <h2>{event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+          {/* make the onClick an anonymous function so that it doesn't just run automatically when the component is rendered */}
         </div>
       ))}
     </div>
-  );
+  )
 }
 
 export default App;
