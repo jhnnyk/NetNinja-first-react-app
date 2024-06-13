49184236922d5cbd5d521f98f107caa93318ab51
John Knotts <jhnnyk@gmail.com>
2024-06-13 07:05:11 -0600

    CSS modules

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import styles from './EventList.module.css'
 
 function EventList({ events, handleClick }) {
   return (
     <div>
       {events.map((event) => (
-        <React.Fragment key={event.id}>
+        <div className={styles.card} key={event.id}>
           <h2>{event.title}</h2>
           <button onClick={() => handleClick(event.id)}>delete event</button>
           {/* make the onClick an anonymous function so that it doesn't just run 
           automatically when the component is rendered */}
-        </React.Fragment>
+        </div>
       ))}
     </div>
   )
